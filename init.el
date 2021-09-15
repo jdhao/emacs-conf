73a79e5da0a21f53d7cb2cbf7e806f8cec817dd6
jdhao <jdhao@hotmail.com>
2021-09-15 22:21:57 +0800

    use UTF-8

--- init.el
+++ init.el
@@ -194,3 +194,6 @@
 ;; auto-reload file if it has been changed outside of Emacs, see
 ;; https://stackoverflow.com/q/1480572/6064933
 (global-auto-revert-mode t)
+
+;; set language environment
+(set-language-environment "UTF-8")
