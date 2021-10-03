178d4253faf07fed792fe3d88ce8035acd7c5bc0
jdhao <jdhao@hotmail.com>
2021-10-04 04:02:45 +0800

    show tab and trailing whitespace

--- init.el
+++ init.el
@@ -348,6 +348,29 @@
 ;; use fancy icons on mode line (currently does not work on Windows)
 ;; (spaceline-all-the-icons-theme)
 
+;; show tabs and trailing whitespace, Ref: http://ergoemacs.org/emacs/whitespace-mode.html
+(require 'whitespace)
+(setq whitespace-display-mappings
+   ;; all numbers are Unicode codepoint in decimal. try (insert-char 182 ) to see it
+  '((space-mark 32 [183] [46]) ; 32 SPACE, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
+    (newline-mark 10 [182 10]) ; 10 LINE FEED
+    (tab-mark 9 [9655 9] [9655 9] [92 9]) ; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
+    ))
+
+(setq whitespace-style '(face tabs trailing tab-mark))
+
+;; change whitespace style
+;; (set-face-attribute 'whitespace-tab nil
+;;                     :background "#f0f0f0"
+;;                     :foreground "#00a8a8"
+;;                     :weight 'bold)
+;; (set-face-attribute 'whitespace-trailing nil
+;;                     :background "#e4eeff"
+;;                     :foreground "#183bc8"
+;;                     :weight 'normal)
+
+(global-whitespace-mode t)
+
 ;; smartparens: auto pair insertion
 (straight-use-package 'smartparens)
 
