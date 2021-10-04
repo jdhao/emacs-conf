aa9b6d4502f25434c887b8d6ce6e79892d2a34a9
jdhao <jdhao@hotmail.com>
2021-10-04 19:42:32 +0800

    add warning about using company-tng-mode

--- init.el
+++ init.el
@@ -250,7 +250,9 @@
 ;; enable company mode in all buffers
 (global-company-mode)
 
-;; use tab and s-tab to cycle forward and backward completion items
+;; use tab and s-tab to cycle forward and backward completion items,
+;; note that after enabling tng-mode, we can not use return to select
+;; the completion item, just press SPC.
 (add-hook 'after-init-hook 'company-tng-mode)
 
 ;; minimum num of typed character to trigger auto-completion
