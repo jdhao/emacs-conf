081798543b40217f66ccb8c8487f17e057786f66
jdhao <jdhao@hotmail.com>
2021-10-07 00:34:02 +0800

    stop using ivy-posframe

--- lisp/init-fuzzy-finder.el
+++ lisp/init-fuzzy-finder.el
@@ -2,7 +2,6 @@
 ;; ivy: fuzzy finder
 (straight-use-package 'ivy)
 (straight-use-package 'counsel)
-(straight-use-package 'ivy-posframe)
 (straight-use-package 'ivy-rich)
 
 ;; enable ivy mode by default
@@ -19,21 +18,22 @@
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 
-;; (require 'ivy-posframe)
+;; (straight-use-package 'ivy-posframe)
+
 ;; display at `ivy-posframe-style'
-(setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display)))
+;; (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display)))
 
-(setq ivy-posframe-parameters
-      '((left-fringe . 4)
-        (right-fringe . 4)))
+;; (setq ivy-posframe-parameters
+;;       '((left-fringe . 4)
+;;         (right-fringe . 4)))
 
-(ivy-posframe-mode 1)
+;; (ivy-posframe-mode 1)
 
 ;; change ivy posframe background color to make it stand out, see
 ;; https://www.reddit.com/r/emacs/comments/jlsass/comment/gas9jaz/?utm_source=share&utm_medium=web2x&context=3
-(set-face-attribute 'ivy-posframe nil
-                    :foreground "white"
-                    ;; list of colors: http://xay-lab.nautilus.xyz/2010/09/emacs.html
-                    :background "#383838")
+;; (set-face-attribute 'ivy-posframe nil
+;;                     :foreground "white"
+;;                     ;; list of colors: http://xay-lab.nautilus.xyz/2010/09/emacs.html
+;;                     :background "#383838")
 
 (provide 'init-fuzzy-finder)
