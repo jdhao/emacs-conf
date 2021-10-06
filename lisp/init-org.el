08e7d349c99b4d9b9cfde47c45036cebc6f2ab10
jdhao <jdhao@hotmail.com>
2021-10-07 00:35:28 +0800

    more org-mode package

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -3,4 +3,12 @@
 (when is-mac
   (straight-use-package 'org-mode))
 
+;; live preview of latex equation
+(straight-use-package 'org-fragtog)
+(add-hook 'org-mode-hook 'org-fragtog-mode)
+
+;; hide elments dynamically
+;; (straight-use-package 'org-appear)
+;; (add-hook 'org-mode-hook 'org-appear-mode)
+
 (provide 'init-org)
