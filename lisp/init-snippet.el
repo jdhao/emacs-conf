2dfa630e54d17fe78729aedde3a7b9139a708591
jdhao <jdhao@hotmail.com>
2021-10-07 00:35:51 +0800

    snippet update

--- lisp/init-snippet.el
+++ lisp/init-snippet.el
@@ -5,18 +5,11 @@
 
 (yas-global-mode 1)
 
-(define-key yas-minor-mode-map (kbd "<tab>") nil)
-(define-key yas-minor-mode-map (kbd "TAB") nil)
-(define-key yas-minor-mode-map (kbd "M-z") 'yas-expand)
+;; (define-key yas-minor-mode-map (kbd "M-z") 'yas-expand)
+;; (define-key yas-minor-mode-map (kbd "<tab>") 'yas-expand)
 
-;; more helpful help window
-(straight-use-package 'helpful)
-
-;; bind keys for helpful
-(global-set-key (kbd "C-h f") #'helpful-callable)
-(global-set-key (kbd "C-h v") #'helpful-variable)
-(global-set-key (kbd "C-h k") #'helpful-key)
-;; Lookup the current symbol at point.
-(global-set-key (kbd "C-c C-d") #'helpful-at-point)
+;; change the shortcut to jump between tab stop poistions.
+;; (define-key yas-keymap (kbd "M-j") 'yas-next-field-or-maybe-expand)
+;; (define-key yas-keymap (kbd "M-k") 'yas-prev-field)
 
 (provide 'init-snippet)
