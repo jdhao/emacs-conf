8f3685233d5a240250856d53759cadefd56511dc
jdhao <jdhao@hotmail.com>
2021-10-08 23:08:51 +0800

    update org settings

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -3,6 +3,16 @@
 (when is-mac
   (straight-use-package 'org-mode))
 
+(setq org-ellipsis " ▼ ")
+
+;; (setq org-src-tab-acts-natively nil)
+
+(straight-use-package 'org-bullets)
+(add-hook 'org-mode-hook #'org-bullets-mode)
+
+(when is-mac
+  (straight-use-package 'org-superstar))
+
 ;; live preview of latex equation
 (straight-use-package 'org-fragtog)
 (add-hook 'org-mode-hook 'org-fragtog-mode)
