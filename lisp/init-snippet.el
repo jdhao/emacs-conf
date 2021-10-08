502717f11ed8d02b5ab6b2c8f9f0b7045d2176b7
jdhao <jdhao@hotmail.com>
2021-10-08 23:08:16 +0800

    fix yasnippet and org-mode error

--- lisp/init-snippet.el
+++ lisp/init-snippet.el
@@ -3,6 +3,10 @@
 (straight-use-package 'yasnippet)
 (straight-use-package 'yasnippet-snippets)
 
+;; avoid error when using yasnippet with org-mode, ref:
+;; https://www.reddit.com/r/emacs/comments/nj08dz/issues_with_yasnippet_in_emacs_272_lisp_error/
+(setq yas-indent-line 'fixed)
+
 (yas-global-mode 1)
 
 ;; (define-key yas-minor-mode-map (kbd "M-z") 'yas-expand)
