7c128b7fc38298455920216cfca085948fcf2086
jdhao <jdhao@hotmail.com>
2021-10-11 22:52:06 +0800

    use prescient for fuzzy search for ivy, no need to use flx

--- lisp/init-fuzzy-finder.el
+++ lisp/init-fuzzy-finder.el
@@ -1,7 +1,7 @@
 
 ;; ivy: fuzzy finder
 (straight-use-package 'ivy)
-(straight-use-package 'flx)
+;; (straight-use-package 'flx)
 (straight-use-package 'counsel)
 (straight-use-package 'ivy-rich) ;; show useful info for ivy items
 (straight-use-package 'all-the-icons-ivy-rich) ;; add fancy icon to ivy UI
@@ -15,8 +15,8 @@
 (ivy-mode 1)
 
 ;; ivy fuzzy searching, ref: https://oremacs.com/2016/01/06/ivy-flx/
-(setq ivy-re-builders-alist
-      '((t . ivy--regex-fuzzy)))
+;; (setq ivy-re-builders-alist
+;;       '((t . ivy--regex-fuzzy)))
 
 ;; or enable counsel mode
 (counsel-mode 1)
@@ -32,14 +32,15 @@
 (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display)))
 
 (setq ivy-posframe-parameters
-      '((left-fringe . 4)
-        (right-fringe . 4)))
+      '((left-fringe . 1)
+        (right-fringe . 1)))
 
 (ivy-posframe-mode 1)
 
-;; change ivy-posframe border color, ref: https://github.com/tumashu/ivy-posframe/issues/83
+;; change ivy-posframe border color, ref: https://github.com/tumashu/ivy-posframe/issues/83.
+;; we can show Emacs builtin colors via command list-colors-display.
 (set-face-attribute 'ivy-posframe-border nil
-                    :background "white")
+                    :background "skyblue")
 
 ;; change ivy posframe background color to make it stand out, see
 ;; https://www.reddit.com/r/emacs/comments/jlsass/comment/gas9jaz/?utm_source=share&utm_medium=web2x&context=3
@@ -47,5 +48,18 @@
 ;;                     :foreground "white"
 ;;                     ;; list of colors: http://xay-lab.nautilus.xyz/2010/09/emacs.html
 ;;                     :background "#383838")
+
+;; prescient can take the place of flx,
+;; ref: https://github.com/raxod502/prescient.el/issues/13
+(straight-use-package 'prescient)
+(straight-use-package 'ivy-prescient)
+
+(setq prescient-filter-method '(initialisum fuzzy))
+(setq prescient-sort-full-matches-first t)
+
+(setq ivy-prescient-retain-classic-highlighting t)
+(ivy-prescient-mode 1)
+
+(prescient-persist-mode)
 
 (provide 'init-fuzzy-finder)
